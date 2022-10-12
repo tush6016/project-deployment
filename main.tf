e284de9a000b188079c91f703a3cf085fc5b5b47
Tushar K <tushars6016@gmail.com>
2022-10-12 23:26:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,15 +3,6 @@ resource "aws_instance" "eks" {
     instance_type = "t3.small"
     key_name = "gen"
     
-    user_data = <<-EOF
-                    #!/bin/bash
-                    sudo yum update -y
-                    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-                    sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-                    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-                    unzip awscliv2.zip
-                    sudo ./aws/install
-                    EOF
                     
     tags = {
       Name = "eks"
