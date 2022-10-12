640840dba763ecea75c44c624f918c825b92e460
Tushar K <tushars6016@gmail.com>
2022-10-12 15:59:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,9 +5,9 @@ resource "aws_instance" "eks" {
     
     user_data = <<-EOF
                     #!/bin/bash
-                    sudo yum install -y yum-utils
-                    sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-                    sudo yum -y install terraform
+                    sudo yum update -y
+                    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+                    sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
                     curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
                     unzip awscliv2.zip
                     sudo ./aws/install
