bb50d59206ce319bb9a014ecaff7af72eb0d7ffc
Tushar K <tushars6016@gmail.com>
2022-10-12 12:03:12 +0530

    added user data

--- main.tf
+++ main.tf
@@ -2,7 +2,13 @@ resource "aws_instance" "eks" {
     ami = "ami-0f924dc71d44d23e2"
     instance_type = "t3.small"
     key_name = "gen"
-
+    
+    user_data = <<EOF
+                    #!/bin/bash
+                    yum install -y yum-utils
+                    yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+                    yum -y install terraform
+                   EOF
     tags = {
       Name = "eks"
     }
