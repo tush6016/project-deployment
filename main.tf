c0145f4b31e20719022a118db5a2f5fd1e90fc9d
Tushar K <tushars6016@gmail.com>
2022-10-12 12:29:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,12 +3,16 @@ resource "aws_instance" "eks" {
     instance_type = "t3.small"
     key_name = "gen"
     
-    user_data = <<EOF
+    user_data = <<-EOF
                     #!/bin/bash
-                    yum install -y yum-utils
-                    yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-                    yum -y install terraform
-                   EOF
+                    sudo yum install -y yum-utils
+                    sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+                    sudo yum -y install terraform
+                    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+                    unzip awscliv2.zip
+                    sudo ./aws/install
+                    EOF
+                    
     tags = {
       Name = "eks"
     }
