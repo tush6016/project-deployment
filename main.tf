dd723f6f3bd34c273517b419ee634669e9562475
Tushar K <tushars6016@gmail.com>
2022-10-14 11:54:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,39 @@
+resource "aws_security_group" "my_security_group" {
+  name        = var.aws_security_group
+  description = "security group for Ec2 instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound from jenkis server
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.aws_security_group
+  }
+}
+
 resource "aws_instance" "eks-master" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  key_name      = var.key_name
+  ami             = var.ami
+  instance_type   = var.instance_type
+  key_name        = var.key_name
+  security_groups = [var.aws_security_group]
 
 
   tags = {
