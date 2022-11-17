31cb272fff3fd021c1360f0d6dcb87366b3b26f2
tush6016 <tushars6016@gmail.com>
2022-11-17 12:53:49 +0530

    CHANGE

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "eks-master" {
-  ami             = var.ami
-  instance_type   = var.instance_type
-  key_name        = var.key_name
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.my_security_group.id]
-  subnet_id       = aws_subnet.subnet-1.id
+  subnet_id              = aws_subnet.subnet-1.id
 
 
 
